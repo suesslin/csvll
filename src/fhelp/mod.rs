c19e75847404ef3ba25c5dc597f25fa6fdafe4a3
Lukas Andreas Muller <lamuller@protonmail.com>
2017-01-21 13:52:03 -0800

    Put file helpers in seperate mod

---
+++ src/fhelp/mod.rs
@@ -0,0 +1,45 @@
+use std::fs::File;
+use std::path::Path;
+use std::error::Error;
+use std::io::prelude::*;
+use std::fs;
+
+// =============================================================================
+//  Helper functions for Files
+// =============================================================================
+
+// QUESTION: Create directory of path if doesn't exist?
+
+// Actually able to create file without using path, just format
+// QUESTION: Any downsides?
+
+pub fn create_file(direc: String, name: String, extension: String) -> File {
+    match File::create(Path::new(&format!("{}/{}.{}", direc, name, extension))) {
+        Ok(file) => file,
+        Err(why) => panic!("{}", why.description())
+    }
+}
+
+pub fn open_file(direc: String, name: String, extension: String) -> File {
+    match File::open(Path::new(&format!("{}/{}.{}", direc, name, extension))) {
+        Ok(file) => file,
+        Err(why) => panic!("{}", why.description())
+    }
+}
+
+pub fn write_to_file(file: &mut File, content: &String) {
+    file.write_all(content.as_bytes());
+}
+
+pub fn read_file(file: &mut File) -> String {
+    let mut content = String::new();
+    match file.read_to_string(&mut content) {
+        Ok(content) => content,
+        Err(why) => panic!("{}", why.description())
+    };
+    content
+}
+
+pub fn delete_file(direc: String, name: String, extension: String) {
+    fs::remove_file(format!("{}/{}.{}", direc, name, extension));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
+mod fhelp;
+
 use std::fs::File;
-use std::path::Path;
-use std::error::Error;
-use std::io::prelude::*;
-use std::fs;
 
 struct Word {
     id: i32,
@@ -30,7 +28,7 @@ struct Table {
 } impl Table {
     fn new(direc: &str, name: &str, ext: &str) -> Table {
         Table {
-            file: open_file(
+            file: fhelp::open_file(
                 direc.to_string(),
                 name.to_string(),
                 ext.to_string()
@@ -41,7 +39,7 @@ struct Table {
     }
 
     fn parse_langs(&mut self) {
-        let mut content = read_file(&mut self.file);
+        let mut content = fhelp::read_file(&mut self.file);
         let lines: Vec<&str> = content.split("\n").collect();
 
         let first_row: Vec<&str> = lines.first().unwrap().split(",").collect();
@@ -92,44 +90,4 @@ fn main() {
         println!("{}", p.val)
     }
 
-}
-
-// =============================================================================
-//  Helper functions for Files
-// =============================================================================
-
-// QUESTION: Create directory of path if doesn't exist?
-
-// Actually able to create file without using path, just format
-// QUESTION: Any downsides?
-
-fn create_file(direc: String, name: String, extension: String) -> File {
-    match File::create(Path::new(&format!("{}/{}.{}", direc, name, extension))) {
-        Ok(file) => file,
-        Err(why) => panic!("{}", why.description())
-    }
-}
-
-fn open_file(direc: String, name: String, extension: String) -> File {
-    match File::open(Path::new(&format!("{}/{}.{}", direc, name, extension))) {
-        Ok(file) => file,
-        Err(why) => panic!("{}", why.description())
-    }
-}
-
-fn write_to_file(file: &mut File, content: &String) {
-    file.write_all(content.as_bytes());
-}
-
-fn read_file(file: &mut File) -> String {
-    let mut content = String::new();
-    match file.read_to_string(&mut content) {
-        Ok(content) => content,
-        Err(why) => panic!("{}", why.description())
-    };
-    content
-}
-
-fn delete_file(direc: String, name: String, extension: String) {
-    fs::remove_file(format!("{}/{}.{}", direc, name, extension));
 }
