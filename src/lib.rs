752ae1a9013372fb8a19352d80433fbde6160a31
Lukas Andreas Muller <lamuller@protonmail.com>
2017-01-22 00:43:23 -0800

    Add lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,56 @@
+mod file_help;
+mod side_models;
+
+use side_models::{Word, Language};
+
+use std::fs::File;
+
+pub struct Manager {
+    pub file: File,
+    pub langs: Vec<Language>,
+    pub words: Vec<Word>
+} impl Manager {
+    pub fn new(direc: &str, name: &str, ext: &str) -> Manager {
+        Manager {
+            file: file_help::open(
+                direc.to_string(),
+                name.to_string(),
+                ext.to_string()
+            ),
+            langs: Vec::new(),
+            words: Vec::new()
+        }
+    }
+
+    pub fn parse_langs(&mut self) {
+        let mut content = file_help::read(&mut self.file);
+        let lines: Vec<&str> = content.split("\n").collect();
+
+        let first_row: Vec<&str> = lines.first().unwrap().split(",").collect();
+
+        // NOTE: Care about performance for parsing words
+        // Old method:
+        // Going through all rows and getting the value at position of current language
+        // New method:
+        // Going through the rows, then languages
+
+        // Word parsing
+        // NOTE: IDs must go -1, because starts counting at 1 (1 should be 0)
+        for word_i in 1..lines.len() {
+            let row_vec: Vec<&str> = lines[word_i].split(",").collect();
+            for lang_id in 1..first_row.len() {
+                match row_vec.get(lang_id) {
+                    Some(val) => { self.words.push(Word::new(word_i as i32 - 1, lang_id as i32 - 1, val));
+                                    // println!("{}", val)
+                                 },
+                    None => println!("Hey")
+                }
+            }
+        }
+
+        // Language parsing
+        for lang_i in 1..first_row.len() {
+            self.langs.push(Language::new(lang_i as i32 - 1, first_row[lang_i]));
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,24 @@
-mod file_help;
-mod side_models;
-
-use side_models::{Word, Language};
-
-use std::fs::File;
-
-pub struct Manager {
-    pub file: File,
-    pub langs: Vec<Language>,
-    pub words: Vec<Word>
-} impl Manager {
-    pub fn new(direc: &str, name: &str, ext: &str) -> Manager {
-        Manager {
-            file: file_help::open(
-                direc.to_string(),
-                name.to_string(),
-                ext.to_string()
-            ),
-            langs: Vec::new(),
-            words: Vec::new()
-        }
-    }
-
-    pub fn parse_langs(&mut self) {
-        let mut content = file_help::read(&mut self.file);
-        let lines: Vec<&str> = content.split("\n").collect();
-
-        let first_row: Vec<&str> = lines.first().unwrap().split(",").collect();
-
-        // NOTE: Care about performance for parsing words
-        // Old method:
-        // Going through all rows and getting the value at position of current language
-        // New method:
-        // Going through the rows, then languages
-
-        // Word parsing
-        // NOTE: IDs must go -1, because starts counting at 1 (1 should be 0)
-        for word_i in 1..lines.len() {
-            let row_vec: Vec<&str> = lines[word_i].split(",").collect();
-            for lang_id in 1..first_row.len() {
-                match row_vec.get(lang_id) {
-                    Some(val) => { self.words.push(Word::new(word_i as i32 - 1, lang_id as i32 - 1, val));
-                                    // println!("{}", val)
-                                 },
-                    None => println!("Hey")
-                }
-            }
-        }
-
-        // Language parsing
-        for lang_i in 1..first_row.len() {
-            self.langs.push(Language::new(lang_i as i32 - 1, first_row[lang_i]));
-        }
-    }
-}
+// fn main() {
+//     let mut t = Manager::new("..", "table", "csv");
+//     t.parse_langs();
+//     for word in t.words {
+//         println!("at {}: {} (lang: {})", word.id, word.val, word.lang_id)
+//     }
+//
+//     // let mut pl: Vec<Word> = Vec::new();
+//     //
+//     // for word in t.words {
+//     //     if word.lang_id == 1 {
+//     //         pl.push(word)
+//     //     }
+//     // }
+//     //
+//     // for p in pl {
+//     //     println!("{}", p.val)
+//     // }
+//
+// }
 
 fn main() {
-    let mut t = Manager::new("..", "table", "csv");
-    t.parse_langs();
-    for word in t.words {
-        println!("at {}: {} (lang: {})", word.id, word.val, word.lang_id)
-    }
-
-    // let mut pl: Vec<Word> = Vec::new();
-    //
-    // for word in t.words {
-    //     if word.lang_id == 1 {
-    //         pl.push(word)
-    //     }
-    // }
-    //
-    // for p in pl {
-    //     println!("{}", p.val)
-    // }
 
 }
